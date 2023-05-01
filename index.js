8cf9d926199d963a5430272ee85b0765bac3ddab
bharath-kadiyala <kadiyalabharath8877@gmail.com>
2023-05-01 11:34:09 +0000

    Super over

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">Super Over league</h1>
+    <div className="img-container">
+      <img
+        className="img-sty"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="img-sty"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
